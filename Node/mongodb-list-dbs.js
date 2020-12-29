d611701a5521d8e8ab9bbdd226a4dd20ff621352
hasan yılmaz <yilmazhasan111@outlook.com>
2020-12-29 21:57:41 +0300

    test mongodb

---
+++ Node/mongodb-list-dbs.js
@@ -0,0 +1,30 @@
+const {MongoClient} = require('mongodb');
+
+async function main() {
+    
+    const conn_str = '';
+       
+    const client = new MongoClient(conn_str);
+    
+    try {
+        await client.connect();
+
+        await listDatabases(client);
+
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+
+} 
+
+async function listDatabases(client){
+    databasesList = await client.db().admin().listDatabases();
+ 
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+ 
+
+main().catch(console.error);
